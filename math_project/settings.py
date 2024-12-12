b477d971f0e2d6398957ae385b3627e2c45b2de9
YuraSirenko <yurii.sirenko.shi.2023@lpnu.ua>
2024-12-12 21:20:28 +0200

    n-th commit + app insights

--- math_project/settings.py
+++ math_project/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k1#253+m^lt7-5dk4_x%&nx9&y6ym(t_t%2nezkm-*jheu17+t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
